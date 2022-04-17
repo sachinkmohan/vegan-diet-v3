6d8e54888ea9f0e66e82f83203ab09492cc6bf1c
developedbyed <ed.magician@gmail.com>
2022-04-17 19:58:04 +0100

    Image optimization

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,12 +1,15 @@
 import styles from "../styles/BlogCard.module.css";
 import moment from "moment";
 import Link from "next/link";
+import Image from "next/image";
 
 function BlogPost({ title, author, coverPhoto, datePublished, slug }) {
   return (
     <div className={styles.card}>
       <Link href={`/posts/${slug}`}>
-        <img src={coverPhoto.url} alt="" />
+        <div className={styles.imgContainer}>
+          <Image layout="fill" src={coverPhoto.url} alt="" />
+        </div>
       </Link>
       <div className={styles.text}>
         <h2>{title}</h2>
