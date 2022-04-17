f96870af040be14e20b66b641fd4a6f7f8667f52
developedbyed <ed.magician@gmail.com>
2022-04-17 21:21:55 +0100

    gif optimize

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -16,10 +16,10 @@ function BlogPost({ title, author, coverPhoto, datePublished, slug }) {
         <div className={styles.details}>
           <div className={styles.author}>
             <img src={author.avatar.url} alt={author.name} />
-            <h5>{author.name}</h5>
+            <h3>{author.name}</h3>
           </div>
           <div className={styles.date}>
-            <h5>{moment(datePublished).format("MMMM d, YYYY")}</h5>
+            <h3>{moment(datePublished).format("MMMM d, YYYY")}</h3>
           </div>
         </div>
       </div>
